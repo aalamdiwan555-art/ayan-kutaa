c8b2463e0df312574863693d28dee6ebbb912c11
aalamdiwan555-art <aalamdiwan555@gmail.com>
2026-08-21 06:59:09 +0530

    Upload project files

---
+++ app/src/main/java/com/autopilot/driver/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.autopilot.driver
+
+import android.app.Application
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppPrefs.init(this)
+        AdManager.init(this)
+    }
+}
